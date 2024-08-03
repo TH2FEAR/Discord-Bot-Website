0bde412a50500562b84c8d624cfd76030e4a0f80
TH2FEAR <80583960+TH2FEAR@users.noreply.github.com>
2024-08-04 00:27:58 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -84,7 +84,7 @@ Invite Seth</a>
 
   <div className="py-10"></div>
   
-  <Link href={"https://discord.gg/ZcDprCrFTn"}>
+  <Link href={"https://discord.gg/cGCbPpasTX"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Support Server</a>
   </Link>
